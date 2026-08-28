035001681d7beeb2dd3cd40d8d92868994713560
sman15tebo-web <sman15tebo.bos@gmail.com>
2026-08-29 06:27:05 +0700

    Update globals.js

--- assets/js/core/globals.js
+++ assets/js/core/globals.js
@@ -1,7 +1,7 @@
 // core/globals.js
 const GAS_URL = "https://script.google.com/macros/s/AKfycbx7Jga_GkctZFoXWmMPsPD2vnhZDE56z_jb31JKm5qKbFKA6Zmo4n-ykEA0hClrI9IXDw/exec";
 
-var loadingInterval = null; var quillPegawaiBerita = null; var tFailsafe = setTimeout(function () { var l = document.getElementById('loader-text'); if (l) { l.innerHTML = "Menunggu respon server...<br><span class='text-xs font-normal'>Sistem sedang merakit data.</span>"; l.style.color = "#f59e0b"; } }, 12000);
+var loadingInterval = null; var quillPegawaiBerita = null; var tFailsafe = setTimeout(function () { var l = document.getElementById('loader-text'); if (l) { l.innerHTML = "Menunggu respon server...<br><span class='text-xs font-normal'>Sistem sedang merakit data.</span>"; l.style.color = "#f59e0b"; } }, 3000);
 
 var isAdmin = false;
 var curRole = sessionStorage.getItem('edupro_role') || '';
